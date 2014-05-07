a8b3ca5a3f8358d6e219c7818140573405850130
leosiu <leonardas.siuksteris@gmail.com>
2014-05-07 22:30:03 +0300

    Pakeista tema ir daug smulkmenu

---
+++ wp-content/themes/flat/footer.php
@@ -0,0 +1,12 @@
+				<?php if ( apply_filters( 'show_flat_credits', true ) ) { ?>
+					<div class="site-info">
+						Powered by Leo
+					</div><!-- .site-info -->
+				<?php } ?>
+			</div>
+		</div>
+	</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
